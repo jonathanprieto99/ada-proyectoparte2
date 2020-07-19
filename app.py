24ef2021326254acc3380db37128ba3f08e54cd0
Jonathan Prieto <jonathan.prieto@me.com>
2020-07-19 09:29:02 -0500

    Subir Dos Imagenes

--- app.py
+++ app.py
@@ -26,27 +26,47 @@ def allowed_file(filename):
 def upload_image():
     filepath = "no"
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file' not in request.files or 'file2' not in request.files:
             return redirect(request.url)
 
         file = request.files['file']
+        file2 = request.files['file2']
         print("File: "+file.filename)
+        print("File2: "+file2.filename)
 
-        if file.filename == '':
+        if file.filename == '' or file2.filename == '':
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
+        if (file and allowed_file(file.filename)) and (file2 and allowed_file(file2.filename)):
+
             filename = secure_filename(file.filename)
+            filename2 = secure_filename(file2.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filepath2 = os.path.join(app.config['UPLOAD_FOLDER'], filename2)
             file.save(filepath)
+            file2.save(filepath2)
+
             im = Image.open(filepath)
             imgblack = im.convert('1')
             np_im = numpy.array(imgblack)
-            #print(np_im)
+            im.close()
+            # print(np_im)
+
             file1 = open("array.txt", "a")
             file1.write(str(np_im))
             file1.close()
-            return render_template("index.html", filename=filepath)
+
+            im2 = Image.open(filepath2)
+            imgblack2 = im2.convert('1')
+            np_im2 = numpy.array(imgblack2)
+            im2.close()
+            #print(np_im2)
+
+            file2 = open("array2.txt", "a")
+            file2.write(str(np_im2))
+            file2.close()
+
+            return render_template("index.html", filename=filepath, filename2=filepath2)
 
     return render_template("index.html", filename=filepath)
 
